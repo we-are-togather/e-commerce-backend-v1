f9686a37909eb328f36b01b9710250531285a80a
Masum181 <masummozumder12@gmail.com>
2026-08-02 19:16:40 +0300

    added checked value to the database file

--- app/core/database.py
+++ app/core/database.py
@@ -7,6 +7,20 @@ from app.core.config import settings
 # engine = create_engine(settings.database_url, future=True)
 # SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
 
+print("========== DATABASE CONFIG ==========")
+print("HOST:", settings.postgres_host)
+print("PORT:", settings.postgres_port)
+print("USER:", settings.postgres_user)
+print("DB:", settings.postgres_db)
+
+print(
+    settings.database_url.replace(
+        settings.postgres_password,
+        "******"
+    )
+)
+print("=====================================")
+
 engine = create_async_engine(settings.database_url)
 
 AsyncSessionLocal = async_sessionmaker(
